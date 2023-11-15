33edcf9e5524e93c066004164c43d67fd402d3a0
romys <romysaputrasihanandaa@gmail.com>
2023-11-15 15:13:20 +0700

    add exceptions

--- src/main/java/com/romys/services/ProductService.java
+++ src/main/java/com/romys/services/ProductService.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import com.romys.exceptions.ProductException;
 import com.romys.models.ProductModel;
 import com.romys.payloads.hit.ElasticHit;
 
@@ -40,8 +41,9 @@ public class ProductService {
                 GetResponse<ProductModel> response = this.client.get(
                                 get -> get.index(this.products).id(id),
                                 ProductModel.class);
-                // if (!response.found())
-                // throw new AlquranException("surat not found");
+
+                if (!response.found())
+                        throw new ProductException("surat not found");
 
                 return new ArrayList<>(List
                                 .of(new ElasticHit<ProductModel>(response.id(), response.index(), response.source())));
