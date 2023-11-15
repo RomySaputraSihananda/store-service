7f39b0d607dd03d4e0f5a756a074aaa98e4be241
romys <romysaputrasihanandaa@gmail.com>
2023-11-15 22:52:45 +0700

    add openAPI configuration

---
+++ src/main/java/com/romys/configurations/OpenAPIConfiguration.java
@@ -0,0 +1,18 @@
+package com.romys.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+@OpenAPIDefinition
+public class OpenAPIConfiguration {
+    @Bean
+    public OpenAPI apiInfo() {
+        return new OpenAPI().info(new Info().title("Store Service Rest API").version("1.0"));
+    }
+
+}
