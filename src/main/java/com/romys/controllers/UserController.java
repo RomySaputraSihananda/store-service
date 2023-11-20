220390248faa61f7e019966752b9df4f86f35e64
romys <romysaputrasihanandaa@gmail.com>
2023-11-20 16:16:16 +0700

    update

--- src/main/java/com/romys/controllers/UserController.java
+++ src/main/java/com/romys/controllers/UserController.java
@@ -7,14 +7,15 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
 
+import com.romys.DTOs.UserDetailDTO;
 import com.romys.models.UserModel;
 import com.romys.payloads.hit.ElasticHit;
 import com.romys.payloads.responses.BodyResponse;
-import com.romys.payloads.responses.BodyResponses;
 import com.romys.services.JwtService;
 import com.romys.services.UserService;
 
@@ -34,22 +35,28 @@ public class UserController {
     @Operation(summary = "Get info self", description = "API for get info self")
     public ResponseEntity<BodyResponse<ElasticHit<UserModel>>> getSelfInfo(HttpServletRequest request)
             throws IOException {
+        String username = request.getHeader(HttpHeaders.AUTHORIZATION);
+
         return new ResponseEntity<>(
-                new BodyResponse<>("ok", HttpStatus.OK.value(), "all data of products",
+                new BodyResponse<>(HttpStatus.OK.getReasonPhrase(), HttpStatus.OK.value(),
+                        String.format("all data from %s", username),
                         this.service.getUserByName(
-                                this.jwtService.extractUsername(request.getHeader(HttpHeaders.AUTHORIZATION)))),
+                                this.jwtService.extractUsername(username))),
                 HttpStatus.OK);
     }
 
-    // @GetMapping("/{id}")
-    // @Operation(summary = "Get user by id", description = "API for get user by
-    // id")
-    // public ResponseEntity<BodyResponse<ElasticHit<UserModel>>>
-    // getById(@PathVariable String id) throws IOException {
-    // return new ResponseEntity<>(
-    // new BodyResponse<>("ok", HttpStatus.OK.value(), String.format("data of users
-    // by id %s", id),
-    // this.service.getUserByid(id)),
-    // HttpStatus.OK);
-    // }
+    @PutMapping
+    @Operation(summary = "Update info self", description = "API for update info self")
+    public ResponseEntity<BodyResponse<ElasticHit<UserModel>>> getById(@RequestBody UserDetailDTO userDetail,
+            HttpServletRequest request) throws IOException {
+        String username = request.getHeader(HttpHeaders.AUTHORIZATION);
+
+        return new ResponseEntity<>(
+                new BodyResponse<>(
+                        HttpStatus.OK.getReasonPhrase(),
+                        HttpStatus.OK.value(),
+                        String.format("success update info %s", username),
+                        null),
+                HttpStatus.OK);
+    }
 }
