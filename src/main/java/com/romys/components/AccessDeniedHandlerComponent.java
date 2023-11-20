6218cd08ef76c16ac45ee0d33d4f5bc231ccdf74
romys <romysaputrasihanandaa@gmail.com>
2023-11-20 11:55:50 +0700

    add components

---
+++ src/main/java/com/romys/components/AccessDeniedHandlerComponent.java
@@ -0,0 +1,35 @@
+package com.romys.components;
+
+import java.io.IOException;
+import java.util.List;
+
+import com.romys.payloads.responses.BodyResponse;
+import org.springframework.http.MediaType;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class AccessDeniedHandlerComponent implements AccessDeniedHandler {
+
+	@Override
+	public void handle(HttpServletRequest request, HttpServletResponse response,
+			AccessDeniedException accessDeniedException) throws IOException, ServletException {
+		response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+		response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+		final ObjectMapper mapper = new ObjectMapper();
+		mapper.writeValue(
+				response.getOutputStream(),
+				new BodyResponse<>(
+						"forbidden", 403, accessDeniedException.getMessage(),
+						List.of(
+								request.getServletPath(),
+								accessDeniedException.getClass().getName())));
+	}
+}
---
+++ src/main/java/com/romys/components/AuthEntryPointComponent.java
@@ -0,0 +1,35 @@
+package com.romys.components;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.romys.payloads.responses.BodyResponse;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class AuthEntryPointComponent implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response,
+            AuthenticationException authException) throws IOException, ServletException {
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        final ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(
+                response.getOutputStream(),
+                new BodyResponse<>(
+                        "unauthorized", 401, authException.getMessage(),
+                        List.of(
+                                request.getServletPath(),
+                                authException.getClass().getName())));
+    }
+}
